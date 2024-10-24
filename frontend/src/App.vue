bdcde815d318a2201b89e4b4a34cbba5f2b6c8dc
Trevan Zhang <trevanzhang@qq.com>
2024-10-24 18:10:51 +0800

    开始手撸硅谷甄选，搞起来

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,62 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <el-button type="primary" size="default" :icon="Plus">点我吧</el-button>
+    <br>
+    <el-button type="danger" size="default" :icon="Plus">点我吧</el-button>
+    <el-calendar v-model="value" />
+    <el-carousel trigger="click" height="150px">
+        <el-carousel-item> container1 </el-carousel-item>
+        <el-carousel-item> container2 </el-carousel-item>
+        <el-carousel-item> container3 </el-carousel-item>
+    </el-carousel>
+    <el-date-picker v-model="value" type="date" placeholder="请选择日期" />
+    <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">promotion management</a></el-breadcrumb-item>
+        <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+        <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[100, 200, 300, 400]"
+        background
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+    />
+    <el-tag :style="{ marginRight: '6px' }">Tag 1</el-tag>
+    <el-tag :style="{ marginRight: '6px' }" type="success">Tag 2</el-tag>
+    <el-tag :style="{ marginRight: '6px' }" type="info">Tag 3</el-tag>
+    <el-tag :style="{ marginRight: '6px' }" type="warning">Tag 4</el-tag>
+    <el-tag :style="{ marginRight: '6px' }" type="danger">Tag 5</el-tag>
+  <el-dialog v-model="show" title="Shipping address">
+      <main>container</main>
+      <template #footer>
+          <span class="dialog-footer">
+              <el-button @click="show = false">Cancel</el-button>
+              <el-button type="primary" @click="show = false"> Confirm </el-button>
+          </span>
+      </template>
+  </el-dialog>
+  <el-date-picker v-model="value" type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" />
+  <br>
+  <el-date-picker v-model="value" type="month" placeholder="请选择月份" />
+  <el-image :src="url" :preview-src-list="[url]" fit="cover" />
+  <el-select v-model="value" multiple collapse-tags placeholder="请选择">
+      <el-option label="Options1" value="1" />
+      <el-option label="Options2" value="2" />
+  </el-select>
+  <el-table :data="tableData" style="width: 100%">
+    <el-calendar v-model="value" />
+  </el-table>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import {Plus} from '@element-plus/icons-vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
-import router from './router'
+import { zhCn } from 'element-plus/es/locales.mjs'
+
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
 app.mount('#app')
